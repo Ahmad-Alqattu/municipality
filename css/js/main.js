81b3241a1d34ff099ce638b7fb16b7c2d03996ec
ahmad qatow <ahmadl.qatu@gmail.com>
2022-10-21 01:52:12 +0300

    new

--- css/js/main.js
+++ css/js/main.js
@@ -1,4 +1,21 @@
-$("heloe").click(function () {
-    $("#hhll").set;
-  });
-  console.log("dafs");
+let hello=document.getElementById('toggele')
+
+hello.onclick=()=>{
+
+  hello.classList.toggle("active")
+}
+
+let btn=document.getElementById('btn');
+
+window.onscroll=()=>{
+if(scrollY >= 850){
+btn.style.display="block"
+}else{
+  btn.style.display="none"
+
+}
+
+}
+btn.onclick=()=>{
+  window.scrollTo(0,0)
+}
